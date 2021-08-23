6e277bad8c00d80d8b21ed5700541da9d86ee254
boxak <boxak@naver.com>
2021-08-24 06:30:15 +0900

    ProducerApp -> stop 누르기 전까지 실행되게 변경

--- src/main/java/com/example/demo/ProducerApp.java
+++ src/main/java/com/example/demo/ProducerApp.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -18,14 +21,18 @@ public class ProducerApp {
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
 
-    Producer<String, String> producer = new KafkaProducer<>(props);
-    ProducerRecord<String, String> record = new ProducerRecord<>("test_topic",null,"Hello World");
-    Future<RecordMetadata> future = producer.send(record);
-
     try {
-      RecordMetadata metadata = future.get();
-      describeMetadata(metadata);
-    } catch (InterruptedException | ExecutionException e) {
+      String msg = "";
+      while(!"stop".equals(msg)) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        msg = br.readLine();
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        ProducerRecord<String, String> record = new ProducerRecord<>("test_topic", null, msg);
+        Future<RecordMetadata> future = producer.send(record);
+        RecordMetadata metadata = future.get();
+        describeMetadata(metadata);
+      }
+    } catch (InterruptedException | ExecutionException | IOException e) {
       e.printStackTrace();
     }
 
